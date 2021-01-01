0c92da3eb6f811eb513c769422e43e692cbfc6ca
niyati salot <170120107148@git.org.in>
2021-01-01 09:54:07 +0530

    Add project files.

---
+++ jaggedArray/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace jaggedArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int[][] jaggedArray = new int[4][];
+            jaggedArray[0] = new int[] { 1, 2, 3 };
+            jaggedArray[1] = new int[] { 100, 200, 300 };
+            jaggedArray[2] = new int[] { 500, 600,700,800,900 };
+            jaggedArray[3] = new int[] { 1000, 2000 };
+            int l = jaggedArray.Length;
+            Console.WriteLine(l);
+            Console.WriteLine(jaggedArray[2].Length);
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+                for (int j = 0; j < jaggedArray[i].Length; j++)
+                {
+                    Console.Write(jaggedArray[i][j] +"\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
